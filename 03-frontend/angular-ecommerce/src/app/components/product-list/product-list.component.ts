f1680e7654f7275327bda8f776a96232d2a1cbb2
anhy167 <ttka1607@gmail.com>
2024-05-24 18:36:01 +0700

    enhance products images

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,8 @@ import { ProdductService } from 'src/app/services/prodduct.service';
 
 @Component({
   selector: 'product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
+  // templateUrl: './product-list-table.component.html',
   // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
