d43a094d28c46f7b404fbe4f7fe53c6a20a83024
anhy167 <ttka1607@gmail.com>
2024-05-26 15:59:31 +0700

    shopping cart status component

--- 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -9,10 +11,10 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-
   product!: Product;
 
   constructor(
+    private cartService: CartService,
     private productService: ProductService,
     private route: ActivatedRoute
   ) {}
@@ -20,16 +22,23 @@ export class ProductDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
       this.handleProductDetails();
-    })
+    });
   }
   handleProductDetails() {
     const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
 
-    this.productService.getProduct(theProductId).subscribe(
-      data => {
-        this.product = data;
-      }
-    )
+    this.productService.getProduct(theProductId).subscribe((data) => {
+      this.product = data;
+    });
     throw new Error('Method not implemented.');
+  }
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, unit price: ${this.product.unitPrice}`);
+    
+    // TODO
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
+    
   }
 }
