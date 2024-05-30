622180f146a0ccf37aa9a020445b30d72fa6c0c6
anhy167 <ttka1607@gmail.com>
2024-05-30 16:18:16 +0700

    save the order to db - backend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@Repository
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 @CrossOrigin
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
