0432f2be6ae4c8b96b5894013325cf3e32cf6e70
anhy167 <ttka1607@gmail.com>
2024-05-31 17:49:21 +0700

    security - login/logout

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oahfersdaWSul3SK5d7',
+        issuer: 'https://dev-45203927.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
