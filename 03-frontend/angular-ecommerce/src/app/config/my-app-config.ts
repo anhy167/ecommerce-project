b631ab5b7052abe1ae3f75061d0627ed85cdd996
anhy167 <ttka1607@gmail.com>
2024-06-07 16:29:27 +0700

    https secure communication - frontend

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oahfersdaWSul3SK5d7',
         issuer: 'https://dev-45203927.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
